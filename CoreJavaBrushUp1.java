a0712ec26a717b1c6c5f3a363b4aad993b269980
Dilip Parthasarathy <dilip.sk@prodapt.com>
2022-04-29 17:23:51 +0530

    modified

--- CoreJavaBrushUp1.java
+++ CoreJavaBrushUp1.java
@@ -3,8 +3,8 @@ package introduction;
 public class CoreJavaBrushUp1 {
 
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,4,5,6,7};
-		arr[6] = 8;
+		int[] arr = {1,2,3,4,5};
+		arr[1] = 8;
 		int length = arr.length;
 		System.out.println(length);
 	}
